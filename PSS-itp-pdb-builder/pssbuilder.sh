34d3c7d161e8ffd5f7668ad026e5476f99987071
HMakkiMD <107540794+HMakkiMD@users.noreply.github.com>
2022-06-15 09:39:25 +0100

    Add files via upload

---
+++ PSS-itp-pdb-builder/pssbuilder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Basic while loop
+counter=1
+numberofchains=60
+chainlength=35
+charge=8
+while [ $counter -le $numberofchains ]
+do
+python3 itpbuilder.py
+mv mypss.itp pss$chainlength-$charge-$counter.itp
+mv mypss.pdb pss3$chainlength-$charge-$counter.pdb
+((counter++))
+done
